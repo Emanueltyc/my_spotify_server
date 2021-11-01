3d886f06947eefc684a035b7c8cf55a9018948ad
Emanuel <jose.eosousa@gmail.com>
2021-11-01 20:52:29 -0300

    add Login route to routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import { LoginController } from './controllers/LoginController';
 import { RefreshTokenController } from './controllers/RefreshTokenController';
 
 const router = Router();
 
+router.post('/login', new LoginController().handle);
 router.post('/refresh', new RefreshTokenController().handle);
 
 export { router };
